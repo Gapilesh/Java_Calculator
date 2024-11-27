f0736afc3d34353fcc2f8c5c58b225bd21ba0e2e
Gapilesh Pirabaharan <108779095+Gapilesh@users.noreply.github.com>
2024-11-28 02:29:37 +0530

    Creating Action Listenenrs and Button arrays

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,28 @@
+// 3 required imports
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+//Implementing an action listener
+public class Calculator implements ActionListener {
+
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButtons = new JButton[10];      // 10 buttons for the 10 numbers
+    JButton[] functionButtons = new JButton[8];     // 8 Function Buttons
+
+    Calculator(){
+
+    }
+
+    public static void main(String[] args){
+
+        //Creating the instance of a calculator class
+        Calculator calc = new Calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
